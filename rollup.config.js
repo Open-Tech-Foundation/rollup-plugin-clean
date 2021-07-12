88904e0335107c24ba41eabe7d645b903ecb10ea
Thanga Ganapathy <ganapathy888@gmail.com>
2021-07-13 00:39:05 +0530

    Add cjs plugin

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,16 @@ import typescript from '@rollup/plugin-typescript';
 export default [
   {
     input: 'src/index.ts',
-    output: {
+    output: [
+      {
         file: 'dist/plugin.esm.js',
         format: 'esm',
       },
+      {
+        file: 'dist/plugin.cjs.js',
+        format: 'cjs',
+      },
+    ],
     external: ['del', 'path', 'globby', 'fs'],
     plugins: [typescript({ tsconfig: './tsconfig.json' })],
   },
